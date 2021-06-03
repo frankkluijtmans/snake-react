d4b88c0efbb76a2e349b5d5d87d1ebfb6a660efd
frankkluijtmans <frank@studyportals.com>
2021-06-03 20:29:40 +0200

    feat: Added scoreboard that displays score and side effects.

--- src/ui/ScoreBoard.tsx
+++ src/ui/ScoreBoard.tsx
@@ -1,15 +1,41 @@
 import React, { Component } from "react";
+import { store } from "../store/store";
 
-export default class ScoreBoard extends Component {
+export default class ScoreBoard extends Component<{}, { 
+    score: number,
+    speed: number,
+    invertedControls: string 
+}>  {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            score: store.getState().gameState.score,
+            speed: store.getState().sideEffects.speedMultiplier,
+            invertedControls: store.getState().sideEffects.invertedControls ? 'Inverted' : 'Normal'
+        };
+
+        this.subscribeToStoreChanges();
+    }
+
+    private subscribeToStoreChanges(): void {
+
+        store.subscribe(() => {
+            this.setState({
+                score: store.getState().gameState.score,
+                speed: store.getState().sideEffects.speedMultiplier,
+                invertedControls: store.getState().sideEffects.invertedControls ? 'Inverted' : 'Normal'
+            });
+        })
     }
 
     render() {
         return (
             <div className="ScoreBoard">
-                <span>Score: 0</span>
+                <span>Score: { this.state.score }</span>
+                <span>Speed: { this.state.speed }</span>
+                <span>Controls: { this.state.invertedControls }</span>
             </div>
         );
     }
