cbb2b2b4b846ef5cb6f41399e01dbea7736531f7
frankkluijtmans <frank_kluyt@hotmail.com>
2021-06-04 21:30:07 +0200

    style: Render gridlines on the plane.

--- src/domain/Plane.ts
+++ src/domain/Plane.ts
@@ -15,5 +15,27 @@ export class Plane {
             .append('svg')
             .attr('width', this.gridSize.width * this.CELL_SIZE)
             .attr('height', this.gridSize.height * this.CELL_SIZE);
+
+		// Render grid lines along the x axis
+		for(let i = 1; i < this.gridSize.width; i++) {
+			this.grid
+				.append('rect')
+				.attr('width', 2)
+				.attr('height', this.gridSize.height * this.CELL_SIZE)
+				.attr('class', 'GridLine')
+				.attr('x', i * this.CELL_SIZE - 1)
+				.attr('y', 0);
+		}
+
+		// Render grid lines along the y axis
+		for(let i = 1; i < this.gridSize.height; i++) {
+			this.grid
+				.append('rect')
+				.attr('width', this.gridSize.width * this.CELL_SIZE)
+				.attr('height', 2)
+				.attr('class', 'GridLine')
+				.attr('x', 0)
+				.attr('y', i * this.CELL_SIZE - 1);
+		}
 	}
 }
